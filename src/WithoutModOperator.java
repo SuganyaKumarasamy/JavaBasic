ad587ccf57fa429225f141fa11389d9fb9e1c413
SuganyaKumarasamy <sugansugi3@gmail.com>
2018-04-24 15:27:50 +0530

    Mode function in seperate method.

--- src/WithoutModOperator.java
+++ src/WithoutModOperator.java
@@ -3,6 +3,18 @@ import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class WithoutModOperator {
+	
+	static String modOperation(int num1, int num2) {
+		if (num2 > num1 || num1 == 0) {
+			return "" + num1;
+		} else {
+			try {
+				return "" + (num1 - (num2 * (num1 / num2)));
+			} catch (ArithmeticException e) {
+				return null;
+			}
+		}
+	}
 
 	public static void main(String[] args) {
 
@@ -14,19 +26,12 @@ public class WithoutModOperator {
 			System.out.println("Enter second number: ");
 			int num2 = in.nextInt();
 			in.close();
-
-			if (num2 > num1) {
-				System.out.println("Remainder: " + num1);
-			} else {
-				try {
-					System.out.println("Remainder: " + (num1 - (num2 * (num1 / num2))));
-				} catch (ArithmeticException e) {
-					System.out.println("Can't be divided by Zero");
-				}
+			String modResult = modOperation(num1, num2);
+			if(modResult != null) {
+				System.out.println("Remainder: " + modResult);
 			}
 		} catch (InputMismatchException e) {
 			System.out.println("Invalid input");
 		}
 	}
-
 }
